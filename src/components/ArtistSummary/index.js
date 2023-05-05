31746edc90a3c484e09f14f6e03027d4706a519b
Ashley Coles <ashley@acoles.co.uk>
2023-05-05 10:50:37 +0100

    Unique key

--- src/components/ArtistSummary/index.js
+++ src/components/ArtistSummary/index.js
@@ -6,7 +6,7 @@ function ArtistSummary({name, albumCount, albumArtwork}) {
         <Link to={"/artists/" + name} className="col-12 col-lg-6 mb-4 text-decoration-none">
             <div className="rounded lightest-blue-hover light-blue p-3 text-light">
                 <div className="justify-content-start mb-2 d-flex">
-                    {albumArtwork.map(artwork => <img key={artwork} src={artwork} className="rounded w-25 me-2" alt="Album artwork" />)}
+                    {albumArtwork.map((artwork, index) => <img key={index} src={artwork} className="rounded w-25 me-2" alt="Album artwork" />)}
                 </div>
                 <h4 className="mb-0">{name}</h4>
                 <small>{albumCount} Albums</small>
